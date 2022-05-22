ea5e47c53d4d2be40fe1ffa6f405063b782f4be5
Brandon Nathan Marks <bmarks7@uwo.ca>
2022-05-22 16:19:13 -0400

    null

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
         <Link to='/find'><h2 className='navbar__find'>Websites</h2></Link>
         <Link to='/findvideo'><h2 className='navbar__findvideo'>Videos</h2></Link>
         <Link to='/'><h1 className='navbar__title'>Buildnow</h1></Link>
-        <Link to='/repo'><h2 className='navbar__repo'>Collections</h2></Link>
+        <Link to='/repo'><h2 className='navbar__repo'>Collection</h2></Link>
         <Link to='/updates'><h2 className='navbar__updates'>Updates</h2></Link>
     </div>
   )
